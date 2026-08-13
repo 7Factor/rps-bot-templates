6c6f140955a73b1867cf1a9634cdfd015ca9d70b
paulskersis <p.skersis@gmail.com>
2026-08-13 13:40:05 -0500

    Naive implementation

--- templates/typescript/team_source/strategy.ts
+++ templates/typescript/team_source/strategy.ts
@@ -5,5 +5,49 @@ export function chooseMove(
   rng: { nextInt(upperExclusive: number): number }
 ): string {
   const moves = ["R", "P", "S"];
-  return moves[rng.nextInt(moves.length)];
+  //Naive cheating to start
+  let returnValue = opposite(moves[rng.nextInt(moves.length)])
+  const [rCount, pCount, sCount] = countOccurence(opponentHistory)
+  let rPerc = rCount/turn
+  let pPerc = pCount/turn
+  let sPerc = sCount/turn
+  if(rPerc > .5){
+    return "P"
+  }
+  else if(pPerc > .5) {
+    return "S"
+  }
+  else if(sPerc > .5) {
+    return "R"
+  }
+  return returnValue;
+}
+
+function opposite(thrown: string
+) {
+  switch (thrown) {
+    case "R":
+      return "P"
+    case "P":
+      return "S"
+    default:
+      return "R"
+  }
+}
+
+function countOccurence(str: string): [number, number, number] {
+  let rCount = 0;
+      let sCount = 0;
+      let pCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "R") {
+      rCount++;
+    } else if (str[i] === "S") {
+      sCount++;
+    } else {
+      pCount++;
+    }
+  }
+
+  return [rCount, pCount, sCount];
 }
