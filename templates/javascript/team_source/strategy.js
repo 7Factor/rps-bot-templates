53c4e03a9c04e6cf19456bcc8fcb6590c7267ab5
Lola Fulton <lola.fulton@7factor.io>
2026-08-13 13:23:44 -0500

    initial commit

--- templates/javascript/team_source/strategy.js
+++ templates/javascript/team_source/strategy.js
@@ -2,6 +2,19 @@
 
 function chooseMove(turn, myHistory, opponentHistory, rng) {
   const moves = ["R", "P", "S"];
+  const rCount = (opponentHistory.match(/R/g)|| []).length
+  const pCount = (opponentHistory.match(/P/g)|| []).length
+  const sCount = (opponentHistory.match(/S/g)|| []).length
+  const turnCount = turn + 1;
+  const threshold = 40;
+
+  if (rCount / turnCount > threshold)
+    return "P";
+  else if (pCount / turnCount > threshold)
+    return "S";
+  else if (sCount / turnCount > threshold)
+    return "R";
+
   return moves[rng.nextInt(moves.length)];
 }
 
